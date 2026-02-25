eb3cbeb3c0f5fd2337805fc7ebe523c9c40b4005
Vitomux <vito.morici94@gmail.com>
2026-02-24 21:02:04 -0300

    subiendo a github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vitomux.github.io',
+  base: '/web-projects',
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
